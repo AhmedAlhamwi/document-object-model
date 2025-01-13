6eb7fb0c44e0381e89394747d2ac0581d1a16db4
AhmedAlhamwi <Alhamwi535@gmail.com>
2025-01-14 00:13:48 +0100

    DOM eerste 3 opdrachten af

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-const title = "Planeten";
-const subTitle = "Alles over planeten";
+const title = document.querySelector(".title") 
+title.textContent= "Planeten";
+const subTitle = document.querySelector(".sub-title") 
+subTitle.textContent = "Alles over planeten";
 const planets = ["Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"];
 const routes = ["Home", "Universum", "Planeten"];
 
-
+const changeContent = document.querySelector(".content-list");
+changeContent.innerHTML = "";
+for (let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    changeContent.innerHTML += `<li> ${planet} </li>`;
+}
 
 
 
